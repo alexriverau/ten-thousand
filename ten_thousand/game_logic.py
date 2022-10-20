11b7bc14ebe1c27f75ebd2ffd152a0c5e65a6a71
Alejandro Rivera <alexrivera78@gmail.com>
2022-10-19 21:58:53 -0500

    pass 8 tests

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class GameLogic:
     pass
 
@@ -12,6 +15,25 @@ class GameLogic:
             number_list.append(roll)
         number_list = tuple(number_list)
         return number_list
+
+    @staticmethod
+    def calculate_score(roll):
+        score = 0
+        counts = Counter(roll).most_common()
+
+        for die in counts:
+            if die[1] >= 3:
+                if die[0] == 1:
+                    score += die[0] * 1000 * (die[1] - 2)
+                else:
+                    score += die[0] * 100 * (die[1] - 2)
+            else:
+                if die[0] == 5:
+                    score += 50*die[1]
+                if die[0] == 1:
+                    score += 100*die[1]
+        return score
+
 
 
 if __name__ == '__main__':
