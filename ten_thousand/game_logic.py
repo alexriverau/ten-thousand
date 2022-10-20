2b9e9f82ce6fee3cdc0aabae4ee48f57fe85066c
Alejandro Rivera <alexrivera78@gmail.com>
2022-10-19 22:29:04 -0500

    lab06 completed

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -7,8 +7,6 @@ class GameLogic:
     @staticmethod
     def roll_dice(dice):
         import random
-        # dice_list = [1, 2, 3, 4, 5, 6]
-        # return_tuple = tuple(random.choice(dice_list))
         number_list = []
         for num in range(dice):
             roll = random.randint(1, 6)
@@ -41,11 +39,3 @@ class GameLogic:
                 if die[0] == 1:
                     score += 100 * die[1]
         return score
-
-
-
-if __name__ == '__main__':
-    print('ran directly as a module')
-#     # to execute run: python -m ten_thousand.game_logic
-#     # Executing without the -m "can" have a different import structure in certain aspects when doing multiple imports
-#     # from different folders
