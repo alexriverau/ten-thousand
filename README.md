ebfbb7a10ea58eb5a4ef06cd391ec1a97a9ac1fc
Alejandro Rivera <alexrivera78@gmail.com>
2022-10-19 20:18:33 -0500

    test_roll_dice passed

---
+++ README.md
@@ -0,0 +1,19 @@
+# LAB - Class 06
+
+---
+
+## Project: Ten Thousand - version 1
+
+### Author: Group 2
+* Monika Davies 
+* Alejandro Rivera
+
+### Links and Resources
+
+### Setup
+
+### Tests
+
+* How do you run tests?
+* Any tests of note?
+* Describe any tests that you did not complete, skipped, etc
---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,21 @@
+class GameLogic:
+    pass
+
+    @staticmethod
+    def roll_dice(dice):
+        import random
+        # dice_list = [1, 2, 3, 4, 5, 6]
+        # return_tuple = tuple(random.choice(dice_list))
+        number_list = []
+        for num in range(dice):
+            roll = random.randint(1, 6)
+            number_list.append(roll)
+        number_list = tuple(number_list)
+        return number_list
+
+
+# if __name__ == '__main__':
+#     print('ran directly as a module')
+#     # to execute run: python -m ten_thousand.game_logic
+#     # Executing without the -m "can" have a different import structure in certain aspects when doing multiple imports
+#     # from different folders
